0b437a11a1b7c55f287ed2f17187ce425e3bcd5d
Tnalxmsk <alstnwkd990@naver.com>
2025-01-15 18:06:03 +0900

    feat: webview 기능 연결

--- App.tsx
+++ App.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { View } from "react-native";
+import { KeyboardAvoidingView, Platform, SafeAreaView, View } from "react-native";
 import WebView from "react-native-webview";
 import { StyleSheet } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 function App(): React.JSX.Element {
 
   return (
-    <View style={styles.container}>
-      <WebView
-        source={{ uri: 'https://your-mode-react.vercel.app/' }}
-        style={styles.webview} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <KeyboardAwareScrollView
+        style={{ flex: 1 }}
+        contentContainerStyle={{ flexGrow: 1 }}
+      >
+        <WebView
+          source={{ uri: 'https://your-mode-react.vercel.app/' }}
+          style={styles.webview}
+          contentInsetAdjustmentBehavior="automatic"
+          keyboardDisplayRequiresUserAction={false}
+          scrollEnabled={true}
+          hideKeyboardAccessoryView={true}
+          allowsInlineMediaPlayback={true}
+        />
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
   );
 }
 
